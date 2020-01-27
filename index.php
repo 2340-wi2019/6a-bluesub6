48a55ceefd2a9fed804b9faf0025a45ae0183eca
bluesub6 <58628866+bluesub6@users.noreply.github.com>
2020-01-26 19:15:12 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "en">
+<!--Jacobi Williams
+        assignment6.htm
+        INFO2340.WW
+        Prof.Thoendel 
+        01/26/20 -->
+<head>
+<title>assignment6</title>
+<meta charset = "utf-8">
+</head>
+<body>
+    <h1>Hello, world!</h1>
+    <div> Allow me to introduce myself! My name is<?php echo "Jacobi Williams"; ?></div>
+    <div> I am currently <?php print "28";?> years old and I am a student at metropolitan community college</div>
+    <div> I am currently studying <?php echo "Information Technology";?> and I hope to graduate in <?php print "2020.";?></div>
+</body>
+
+</html>
